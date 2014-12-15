df67a335dae31a575ebc571ccf1e08aba9033f42
langostinko <langostinko@gmail.com>
2014-12-15 17:28:35 +0300

    [lib] esarev translate quality

--- lib/core.php
+++ lib/core.php
@@ -29,7 +29,7 @@
             array("ЗВУК С TS","ЗВУК С CAMRIP",)
         )) return 1;
         if (in_array($qual,
-            array("L","L1","L2","A",)
+            array("L","L1","L2","A","ЕСАРЕВ")
         )) return 2;
         if (in_array($qual,
             array("P","P2","BAIBAKO",)
@@ -84,7 +84,7 @@
 
     function extractTranslate($str, &$movie){
     	$result = array();
-    	$res = preg_match_all('/\| *[\W](лицензия|чистый звук|звук с ts|Звук с CAMRip|iTunes|BaibaKo)[\W]/isuU', $str.' ', $result);
+    	$res = preg_match_all('/\| *[\W](лицензия|чистый звук|звук с ts|Звук с CAMRip|iTunes|BaibaKo|Есарев)[\W]/isuU', $str.' ', $result);
     	if (!$result[0])
         	$res = preg_match_all('/\| *[\W](l|l1|l2|p|p2|D|A)[\W]/isuU', $str.' ', $result);
     	if ($result[0])
