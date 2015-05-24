0eb2a092e567eefc9eb8c3cf959ee6f1cc3723f3
langostinko <langostinko@gmail.com>
2015-05-24 23:18:58 +0300

    new possible rutor root

--- lib/defines.php
+++ lib/defines.php
@@ -4,6 +4,7 @@
     define("ADDLINKSPASTDAYS", 180);
     define("FRESHLINKSDAYS", 14);
     define("RUTORROOT", "http://zerkalo-rutor.org");
+    //define("RUTORROOT", "http://new-rutor.org");
     //define("PROXY", "113.255.61.57:80");
     define("PROXY", "183.207.229.200:80");
     //define("PROXY", null);
