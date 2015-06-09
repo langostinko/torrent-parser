7a39ba1f2a5c971c16cbdd0eb040945e827e2400
langostinko <langostinko@gmail.com>
2015-06-09 18:04:33 +0300

    short names fix

--- lib/loaders/RutorLoader.php
+++ lib/loaders/RutorLoader.php
@@ -25,7 +25,7 @@ class RutorLoader extends AbstractLoader {
         $title = html_entity_decode($res->plaintext, ENT_QUOTES, "UTF-8");
         $movie['description'] = $title;
         $pos = strrpos($title, '/') + 1;
-        if ($pos != 1)
+        if ($pos > 4)
             $title = trim(substr($title, $pos));
 
         $result = array();
