86964baac9175879bfe0da3f30f9425c8a45f068
Dmitry Yandimirkin <langostinko@gmail.com>
2016-02-29 22:58:54 +0300

    donate button

--- charity.php
+++ charity.php
@@ -31,11 +31,25 @@
     <!-- Main jumbotron for a primary marketing message or call to action -->
     <div id='main' class="container-fluid" style="padding: 0">
         <h2>Очищение кармы</h2>
+        <form class="form-inline" action='https://money.yandex.ru/quickpay/confirm.xml' method='post' target='_blank'>  
+            <input type='hidden' name='receiver' value='410011013406408'/>
+            <!--input type='hidden' name='formcomment' value='formcomment'/-->
+            <!--input type='hidden' name='short-dest' value='short-dest'/-->
+            <input type='hidden' name='quickpay-form' value='donate'/>
+            <input type='hidden' name='paymentType' value='AC'/>
+            <div class="input-group">
+                <input class="form-control" type="number" name='sum' value='50' step='50' style="width: 70px"/>
+                <div class="input-group-addon">₽</div>
+            </div>
+            <input class="form-control" type='text' name='targets' value='<?php $tmp=array("на кофе", "на пиццу", "на печеньки", "на пивко", "на лимонад");shuffle($tmp);echo reset($tmp);?>'/>
+            <button type="submit" class="btn btn-success">поддержать создателей</button>
+        </form>
+        <br/>
+        <p>или пожертвуйте 50₽ в один из фондов</p>
         <?php
             define("allCharity", true);
             include "html/charity.php";
         ?>
-        <p>пожертвуйте 50₽ в один из фондов</p>
     </div>
     <?php
         include "html/footer.php";
