5178ed0553f0124c027805634477ba50872293d0
Dmitry Yandimirkin <langostinko@gmail.com>
2018-06-28 23:52:29 +0300

    fix rutor

--- lib/defines.php
+++ lib/defines.php
@@ -8,7 +8,7 @@
     define("FRESHLINKSDAYS", 21);
     define("LOGDIR", __DIR__."/../logs/");
 
-    define("RUTORROOT", "http://nerutor.org");
+    define("RUTORROOT", "http://d.zator.org");
     define("NNMROOT", "http://nnmclub.to");
     define("PIRATEROOT", "https://thepiratebay.plus");
     define("KINOPOISKROOT", "https://www.kinopoisk.ru");
